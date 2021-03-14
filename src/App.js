621aeef51b426402815e04e4a27ccb89d6ff1ee4
Ashish <yeshu.ya1716@Gmail.com>
2021-03-14 08:37:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [ashish, setAshish] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h1>Counter app</h1>
+        <h2>Current value is {ashish}</h2>
+        <button onClick={() => (ashish > 9 ? "" : setAshish(ashish + 1))}>
+          +
+        </button>
+        <button onClick={() => setAshish(0)}>0</button>
+        <button onClick={() => (ashish < 1 ? "" : setAshish(ashish - 1))}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
